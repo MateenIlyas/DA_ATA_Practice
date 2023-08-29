b95314992f6bf8fe420481ebdff50eba1c089374
Mateen Ilyas <Mateen_Raja2001@yahoo.com>
2023-08-29 21:56:09 +0500

    Update SQL-Intermediate-ATA-P3.sql

--- SQL-Intermediate-ATA-P3.sql
+++ SQL-Intermediate-ATA-P3.sql
@@ -27,3 +27,48 @@ WHERE EmployeeID = 1005
 /* Then using the DELETE statement to delete the record */
 DELETE FROM master.dbo.EmployeeDemographics
 WHERE EmployeeID = 1005
+
+/* This allows us to shorten the column names and show them in a congested space */
+/* Here, we take the average of employee ages, and put it in a column called AvgAge */
+SELECT AVG(Age) AS AvgAge
+FROM master.dbo.EmployeeDemographics
+
+/* This allows us to shorten the column names and show them in a congested space */
+/* Here, we have given aliases to the columns as well as the tables */
+/* First, we gave alias DEMO to the table EmployeeDemographics */
+/* Second, the two columns are combined and their new name is FullName */
+SELECT DEMO.FirstName + ' ' + Demo.LastName AS FullName
+FROM master.dbo.EmployeeDemographics AS DEMO
+
+/* This allows us to shorten the column names and show them in a congested space */
+/* Here, we gave alias DEMO to the table EmployeeDemographics */
+SELECT DEMO.EmployeeID
+FROM master.dbo.EmployeeDemographics AS DEMO
+
+/* Here we gave aliases to two tables */
+/* Then we joined them on EmployeeID column */
+/* Then we chose to display two columns i.e. EmployeeID from DEMO and Salary from Sal */
+SELECT DEMO.EmployeeID, Sal.Salary
+FROM master.dbo.EmployeeDemographics AS DEMO
+JOIN master.dbo.EmployeeSalary AS Sal
+	ON DEMO.EmployeeID = Sal.EmployeeID
+
+/* This query is not a good way to use aliases as it lacks context and clarity */
+/* It will create confusion about which table is a, or b, or c */
+SELECT a.EmployeeID, a.FirstName, a.LastName, a.Age, a.Gender
+FROM master.dbo.EmployeeDemographics AS a
+LEFT JOIN master.dbo.EmployeeSalary AS b
+	ON a.EmployeeID = b.EmployeeID
+LEFT JOIN master.dbo.WareHouseEmployeeDemographics AS c
+	ON a.EmployeeID = c.EmployeeID
+
+/* This query provides context and clarity about all three tables */
+/* DEMO is the alias for EmployeeDemographics */
+/* Sal is the alias for EmployeeSalary */
+/* Ware is the alias for WareHouseEmployeeDemographics */
+SELECT DEMO.EmployeeID, DEMO.FirstName, DEMO.LastName, DEMO.Age, DEMO.Gender
+FROM master.dbo.EmployeeDemographics AS DEMO
+LEFT JOIN master.dbo.EmployeeSalary AS Sal
+	ON DEMO.EmployeeID = Sal.EmployeeID
+LEFT JOIN master.dbo.WareHouseEmployeeDemographics AS Ware
+	ON DEMO.EmployeeID = Ware.EmployeeID
